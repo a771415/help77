ce8264dac88f97565d9600e86e402348cc2a05e5
a771415 <136606953+a771415@users.noreply.github.com>
2023-11-13 19:24:12 +0300

    dfg
    dfg

---
+++ .github/workflows/header.php
@@ -0,0 +1,25 @@
+<!-- header.php -->
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles.css">
+    <title><?php echo $pageTitle; ?></title>
+</head>
+
+<body>
+    <header>
+        <div class="logo">
+            <h1 class="logo-text">شعار الموقع</h1>
+        </div>
+        <nav>
+            <ul>
+                <li><a href="index.php">الرئيسية</a></li>
+                <li><a href="services.php">الخدمات</a></li>
+                <li><a href="products.php">المنتجات</a></li>
+                <li><a href="contact.php">التواصل</a></li>
+            </ul>
+        </nav>
+    </header>
---
+++ .github/workflows/index.php
@@ -0,0 +1,22 @@
+<!-- index.php -->
+<?php include('header.php'); ?>
+<?php include('sidebar.php'); ?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles.css">
+    <title>الصفحة الرئيسية</title>
+</head>
+
+<body>
+    <div class="container">
+        <div class="content">
+            <!-- Your content goes here -->
+        </div>
+    </div>
+</body>
+
+</html>
